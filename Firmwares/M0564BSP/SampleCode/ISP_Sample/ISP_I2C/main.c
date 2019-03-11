0fb4023a8c29e706d67225b64d181f48163dee04
CCTu <CCTu@nuvoton.com>
2019-03-11 10:39:32 +0800

    Updated M0564 ISP FW_VER to 0x34 & Added ISP_I2C Sample

--- Firmwares/M0564BSP/SampleCode/ISP_Sample/ISP_I2C/main.c
+++ Firmwares/M0564BSP/SampleCode/ISP_Sample/ISP_I2C/main.c
@@ -1,49 +1,54 @@
-#include "stdio.h"
+#include <stdio.h>
 #include "targetdev.h"
 
-#define PLLCTL_SETTING      CLK_PLLCTL_72MHz_HIRC
-#define PLL_CLOCK           71884800
-/*---------------------------------------------------------------------------------------------------------*/
-/* Define functions prototype                                                                              */
-/*---------------------------------------------------------------------------------------------------------*/
-int32_t main(void);
+#define PLLCTL_SETTING  CLK_PLLCTL_72MHz_HXT
+#define PLL_CLOCK       72000000
 
 void SYS_Init(void)
 {
     /*---------------------------------------------------------------------------------------------------------*/
     /* Init System Clock                                                                                       */
     /*---------------------------------------------------------------------------------------------------------*/
-    /* Enable HIRC clock (Internal RC 22.1184MHz) and HXT clock (external XTAL 12MHz) */
-    CLK->PWRCTL |= (CLK_PWRCTL_HIRCEN_Msk | CLK_PWRCTL_HXTEN_Msk);
+    /* Enable HIRC clock (Internal RC 22.1184MHz) */
+    CLK->PWRCTL |= CLK_PWRCTL_HIRCEN_Msk;
 
-    /* Wait for HIRC clock ready */
+    /* Waiting for HIRC clock ready */
     while (!(CLK->STATUS & CLK_STATUS_HIRCSTB_Msk));
 
-    /* Set core clock as PLL_CLOCK from PLL and HCLK clock divider as 1 */
+    /* Select HCLK clock source as HIRC and and HCLK clock divider as 1 */
+    CLK->CLKSEL0 &= ~CLK_CLKSEL0_HCLKSEL_Msk;
+    CLK->CLKSEL0 |= CLK_CLKSEL0_HCLKSEL_HIRC;
+    CLK->CLKDIV0 &= ~CLK_CLKDIV0_HCLKDIV_Msk;
+    CLK->CLKDIV0 |= CLK_CLKDIV0_HCLK(1);
+    /* Enable HXT clock (external XTAL 12MHz) */
+    CLK->PWRCTL |= CLK_PWRCTL_HXTEN_Msk;
+
+    /* Waiting for HXT clock ready */
+    while (!(CLK->STATUS & CLK_STATUS_HXTSTB_Msk));
+
+    /* Set core clock as PLL_CLOCK from PLL */
     CLK->PLLCTL = PLLCTL_SETTING;
 
     while (!(CLK->STATUS & CLK_STATUS_PLLSTB_Msk));
 
-    //CLK->CLKSEL0 &= (~CLK_CLKSEL0_HCLKSEL_Msk);
-    //CLK->CLKSEL0 |= CLK_CLKSEL0_HCLKSEL_PLL;
-    CLK->CLKSEL0 = (CLK->CLKSEL0 & (~CLK_CLKSEL0_HCLKSEL_Msk)) | CLK_CLKSEL0_HCLKSEL_PLL;
-    CLK->CLKDIV0 &= ~CLK_CLKDIV0_HCLKDIV_Msk;
-    CLK->CLKDIV0 |= CLK_CLKDIV0_HCLK(1);
+    CLK->CLKSEL0 &= (~CLK_CLKSEL0_HCLKSEL_Msk);
+    CLK->CLKSEL0 |= CLK_CLKSEL0_HCLKSEL_PLL;
     /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate PllClock, SystemCoreClock and CycylesPerUs automatically. */
     PllClock        = PLL_CLOCK;            // PLL
     SystemCoreClock = PLL_CLOCK / 1;        // HCLK
     CyclesPerUs     = PLL_CLOCK / 1000000;  // For CLK_SysTickDelay()
-    /* Enable UART module clock */
-    CLK->APBCLK0 |= CLK_APBCLK0_UART0CKEN_Msk;
-    /* Select UART module clock source as HIRC and UART module clock divider as 1 */
-    CLK->CLKSEL1 = (CLK->CLKSEL1 & (~CLK_CLKSEL1_UARTSEL_Msk)) | CLK_CLKSEL1_UARTSEL_HIRC;
-    CLK->CLKDIV0 = (CLK->CLKDIV0 & (~CLK_CLKDIV0_UARTDIV_Msk)) | CLK_CLKDIV0_UART(1);
+    /* Enable I2C controller */
+    CLK->APBCLK0 |= CLK_APBCLK0_I2C0CKEN_Msk;
     /*---------------------------------------------------------------------------------------------------------*/
     /* Init I/O Multi-function                                                                                 */
     /*---------------------------------------------------------------------------------------------------------*/
-    /* Set PD multi-function pins for UART0 RXD(PD.0) and TXD(PD.1) */
-    SYS->GPD_MFPL = (SYS->GPD_MFPL & (~SYS_GPD_MFPL_PD0MFP_Msk)) | SYS_GPD_MFPL_PD0MFP_UART0_RXD;
-    SYS->GPD_MFPL = (SYS->GPD_MFPL & (~SYS_GPD_MFPL_PD1MFP_Msk)) | SYS_GPD_MFPL_PD1MFP_UART0_TXD;
+    /* Set I2C P3 multi-function pins */
+    SYS->GPD_MFPL &= ~(SYS_GPD_MFPL_PD4MFP_Msk | SYS_GPD_MFPL_PD5MFP_Msk);
+    SYS->GPD_MFPL |= (SYS_GPD_MFPL_PD4MFP_I2C0_SDA | SYS_GPD_MFPL_PD5MFP_I2C0_SCL);
+    /* Set ReadyPin to Output Mode */
+    PD->MODE = (PD->MODE & (~GPIO_MODE_MODE3_Msk)) | (GPIO_MODE_OUTPUT << GPIO_MODE_MODE3_Pos);
+    ReadyPin = 1;
 }
 
 /*---------------------------------------------------------------------------------------------------------*/
@@ -57,26 +62,17 @@ int32_t main(void)
     SYS_Init();
     WDT->CTL &= ~(WDT_CTL_WDTEN_Msk | WDT_CTL_ICEDEBUG_Msk);
     WDT->CTL |= (WDT_TIMEOUT_2POW18 | WDT_CTL_RSTEN_Msk);
-    /* Init UART0 for printf and test */
-    UART_Init();
     FMC->ISPCTL |=  FMC_ISPCTL_ISPEN_Msk;
     g_apromSize = GetApromSize();
     GetDataFlashInfo(&g_dataFlashAddr, &g_dataFlashSize);
+    I2C_Init();
     SysTick->LOAD = 300000 * CyclesPerUs;
     SysTick->VAL   = (0x00);
     SysTick->CTRL = SysTick->CTRL | SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;//using cpu clock
 
     while (1) {
-        if ((bufhead >= 4) || (bUartDataReady == TRUE)) {
-            uint32_t lcmd;
-            lcmd = inpw(uart_rcvbuf);
-
-            if (lcmd == CMD_CONNECT) {
-                break;
-            } else {
-                bUartDataReady = FALSE;
-                bufhead = 0;
-            }
+        if (bI2cDataReady == 1) {
+            goto _ISP;
         }
 
         if (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) {
@@ -84,13 +80,16 @@ int32_t main(void)
         }
     }
 
+_ISP:
+
     while (1) {
-        if (bUartDataReady == TRUE) {
+        if (bI2cDataReady == 1) {
             WDT->CTL &= ~(WDT_CTL_WDTEN_Msk | WDT_CTL_ICEDEBUG_Msk);
             WDT->CTL |= (WDT_TIMEOUT_2POW18 | WDT_CTL_RSTEN_Msk);
-            bUartDataReady = FALSE;
-            ParseCmd(uart_rcvbuf, 64);
-            PutString();
+            bI2cDataReady = 0;
+            ParseCmd(i2c_rcvbuf, 64);
+            bISPDataReady = 1;
+            ReadyPin = 0;
         }
     }
 
